7ca2f0936ce69f3f6ea04b7a674fefc8f50c7de8
Ivano Malavolta <ivanomalavolta@gmail.com>
2014-05-27 14:42:39 +0200

    Bug fix
    The myModel module was mistakenly referred as Handlebars

--- js/collections/MyCollection.js
+++ js/collections/MyCollection.js
@@ -3,7 +3,7 @@ define(function(require) {
 	var $ = require("jquery");
 	var _ = require("underscore");
 	var Backbone = require("backbone");
-	var Handlebars = require("models/MyModel");
+	var MyModel = require("models/MyModel");
 
 	var MyCollection = Backbone.Collection.extend({
 		constructorName: "MyCollection",
